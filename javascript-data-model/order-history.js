3f04bdae88c44a41da1d158ef2e56d377cfcda4c
Paul Park <paulpark330@hotmail.com>
2021-06-29 11:02:43 -0700

    completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,71 @@
+var orderHistory = [
+  {
+    order: {
+      orderNumber: '114-3941689-8772232',
+      orderDate: 'August 4, 2020',
+      orderTotal: '$34.00',
+      numberOfItems: 1
+    },
+    items: {
+      itemName: ['JavaScript for impatient programmers'],
+      itemPrice: ['$31.55'],
+      author: ['Rauschmayer, Dr. Axel'],
+      deliveredDate: ['Aug 8, 2020'],
+      returnWindow: [
+        { closed: true, date: 'Aug,4, 2020' }
+      ]
+    }
+  },
+  {
+    order: {
+      orderNumber: '113-9984264-1280257',
+      orderDate: 'July 19',
+      orderTotal: '$44.53',
+      numberOfItems: 1
+    },
+    items: {
+      itemName: ['The Timeless Way of Building'],
+      itemPrice: ['$41.33'],
+      author: ['Alexander, Christopher'],
+      deliveredDate: ['Jul 20, 2020'],
+      returnWindow: [
+        { closed: true, date: 'Aug,4, 2020' }
+      ]
+    }
+  },
+  {
+    order: {
+      orderNumber: '114-2875557-9059409',
+      orderDate: 'July 4',
+      orderTotal: '$17.22',
+      numberOfItems: 1
+    },
+    items: {
+      itemName: ['Gamecube Controller Adapter'],
+      itemPrice: ['$15.98'],
+      author: [null],
+      deliveredDate: ['Jul 7, 2020'],
+      returnWindow: [
+        { closed: true, date: 'Aug,4, 2020' }
+      ]
+    }
+  },
+  {
+    order: {
+      orderNumber: '113-2883177-2648248',
+      orderDate: 'July 3',
+      orderTotal: '$138.93',
+      numberOfItems: 2
+    },
+    items: {
+      itemName: ['GameCube Controller', 'The Art of Sql'],
+      itemPrice: ['$94.95', '$33.99'],
+      author: [null, 'Faroult, Stephane'],
+      deliveredDate: ['Jul 5, 2020', 'Jul 5,2020'],
+      returnWindow: [
+        { closed: true, date: 'Aug,4, 2020' },
+        { closed: true, date: 'Aug,4, 2020' }
+      ]
+    }
+  }
+];
