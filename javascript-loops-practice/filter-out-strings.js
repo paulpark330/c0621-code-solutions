f33b47cf8d919174859e1a3c971101358a4c01f8
Paul Park <paulpark330@hotmail.com>
2021-07-01 16:58:33 -0700

    modified filterOutStrings function with pseudocode

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -2,9 +2,19 @@
 function filterOutStrings(values) {
   var newArr = [];
   for (let i = 0; i < values.length; i++) {
-    if (typeof values[i] === 'number') {
+    if (typeof values[i] === 'string') {
+      continue;
+    } else {
       newArr.push(values[i]);
     }
   }
   return newArr;
 }
+
+// New storage for all of the items in the list except strings.
+// Look at each item one at a item
+//    IF the item is a string
+//      Do not put that item in the new storage and move on
+//    ELSE otherwise
+//      Put the item in the new storage
+// Give back the new storage
