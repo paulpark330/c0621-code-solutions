2fdaf691fc148e8b8c9e9af72e2c758878622c6e
Paul Park <85209433+paulpark330@users.noreply.github.com>
2021-07-01 17:45:41 -0700

    Updated if statement conditions and code block
    Updated pseudocode

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -2,8 +2,7 @@
 function filterOutStrings(values) {
   var newArr = [];
   for (let i = 0; i < values.length; i++) {
-    if (typeof values[i] === 'string') {
-    } else {
+    if (typeof values[i] !== 'string') {
       newArr.push(values[i]);
     }
   }
@@ -12,8 +11,6 @@ function filterOutStrings(values) {
 
 // New storage for all of the items in the list except strings.
 // Look at each item one at a item
-//    IF the item is a string
-//      Do not put that item in the new storage and move on
-//    ELSE otherwise
+//    IF the item is not string
 //      Put the item in the new storage
 // Give back the new storage
