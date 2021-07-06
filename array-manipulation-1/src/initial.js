e87f0cda7bd83f607b8d00f40532c387e6b20f13
Paul Park <85209433+paulpark330@users.noreply.github.com>
2021-07-06 11:08:43 -0700

    updated condition

--- array-manipulation-1/src/initial.js
+++ array-manipulation-1/src/initial.js
@@ -1,13 +1,12 @@
 /* exported initial */
 //  New list containing all items except the last item
-//  Go through every item in the list
-//    IF the item is not the last item, put it in the list
+//  Go through every item in the list except for the last item
+//    Put item in the list
 //  Give back the new list
 
 function initial(array) {
   var newArr = [];
-  for (let i = 0; i < array.length; i++) {
-    if (i !== array.length - 1) {
+  for (let i = 0; i < array.length - 1; i++) {
       newArr.push(array[i]);
     }
   }
