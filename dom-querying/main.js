140116291473f1753dec57cc5bfd6f704b408b4a
Paul Park <paulpark330@hotmail.com>
2021-07-06 12:19:52 -0700

    updated dom-querying

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,8 +1,17 @@
-console.log('$heading', document.querySelector('h1'));
-console.dir(document.querySelector('h1'));
-console.log('$explanation', document.querySelector('#explanation'));
-console.dir(document.querySelector('#explanation'));
-console.log('$hint', document.querySelector('.hint'));
-console.dir(document.querySelector('.hint'));
-console.log('$paragraphs', document.querySelectorAll('p'));
-console.log('$links', document.querySelectorAll('.example-link'));
+var $heading = document.querySelector('h1');
+console.log('$heading', $heading);
+console.dir($heading);
+
+var $explanation = document.querySelector('#explanation');
+console.log('$explanation', $explanation);
+console.dir($explanation);
+
+var $hint = document.querySelector('.hint');
+console.log('$hint', $hint);
+console.dir($hint);
+
+var $paragraphs = document.querySelectorAll('p');
+console.log('$paragraphs', $paragraphs);
+
+var $links = document.querySelectorAll('.example-link');
+console.log('$links', $links);
