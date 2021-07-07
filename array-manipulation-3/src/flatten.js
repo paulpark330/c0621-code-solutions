7487c346bd64bc06e7bf9f678fe3ada3bec11646
Paul Park <paulpark330@hotmail.com>
2021-07-07 11:12:01 -0700

    working on flattn.js

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,12 @@
 /* exported flatten */
+
+//  EDGE CASES
+
+//  How many layers of arrays?
+
+//  PSEUDOCODE
+
+//  Storage for new array containing unwrapped items
+//  Go through each item in the array
+//    IF item is an array
+//    Go through each item
