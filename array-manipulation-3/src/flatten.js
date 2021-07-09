31e146f6f249d58c70ec4d2f97b808425b8499ae
Paul Park <paulpark330@hotmail.com>
2021-07-09 12:28:29 -0700

    incomplete

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -10,3 +10,21 @@
 //  Go through each item in the array
 //    IF item is an array
 //    Go through each item
+//      Put item in new array
+//    ELSE otherwise
+//      Put item in new array
+//  Return new array
+
+function flatten(array) {
+  var newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        newArr.push(array[i][j]);
+      }
+    } else {
+      newArr.push(array[i]);
+    }
+  }
+  return newArr;
+}
--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,11 @@
 /* exported zip */
+
+//  PSEUDOCODE
+
+//  New array containing an array of pairs from two arguments
+//  Loop through the first argument
+//    New sub array for the pairs
+//    Push item in the new array
+//  Loop through the second argument
+//    IF an item already exists at the index at new array
+//      Push the item in the sub array in the new array
