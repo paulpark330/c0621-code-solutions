bcf86358289c52293bf5b35b38cbbe2c4a17eb97
Paul Park <paulpark330@hotmail.com>
2021-07-09 15:25:43 -0700

    completed array-manipulation-3

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,27 @@
 /* exported difference */
+
+//  PSEUDOCODE
+
+//  New array symmetric difference array
+//  Loop over the first array
+//    IF item doesn't exist in the second array
+//      Push item in new array
+//  Loop over the second array
+//    If item doesn't exist in the first array
+//      Push item in the new array
+//  Return new array
+
+function difference(first, second) {
+  var newArr = [];
+  for (let i = 0; i < first.length; i++) {
+    if (!second.includes(first[i])) {
+      newArr.push(first[i]);
+    }
+  }
+  for (let i = 0; i < second.length; i++) {
+    if (!first.includes(second[i])) {
+      newArr.push(second[i]);
+    }
+  }
+  return newArr;
+}
--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -4,8 +4,23 @@
 
 //  New array containing an array of pairs from two arguments
 //  Loop through the first argument
-//    New sub array for the pairs
-//    Push item in the new array
-//  Loop through the second argument
-//    IF an item already exists at the index at new array
-//      Push the item in the sub array in the new array
+//    Loop through the second argument
+//      IF both indices are the same
+//        New array for the pairs
+//        Put both items from first and second to the pair array
+//        Put pair array in new array
+//  Return new array
+
+function zip(first, second) {
+  var newArr = [];
+  for (let i = 0; i < first.length; i++) {
+    for (let j = 0; j < second.length; j++) {
+      if (i === j) {
+        var pairArr = [];
+        pairArr.push(first[i], second[i]);
+        newArr.push(pairArr);
+      }
+    }
+  }
+  return newArr;
+}
