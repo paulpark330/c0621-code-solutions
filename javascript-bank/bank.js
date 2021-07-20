5cd78ea05699190631aafcb5748c324b9ad2594c
Paul Park <paulpark330@hotmail.com>
2021-07-20 11:15:37 -0700

    completed javascript-bank

---
+++ javascript-bank/bank.js
@@ -0,0 +1,38 @@
+/* exported Bank */
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (balance > 0 && Number.isInteger(balance)) {
+    var account = new Account(this.nextAccountNumber, holder);
+    account.deposit(balance);
+    this.accounts.push(account);
+    this.nextAccountNumber++;
+    return account.number;
+  } else {
+    return null;
+  }
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (let i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  if (this.accounts.length === 0) {
+    return 0;
+  } else {
+    var totalAssets = 0;
+    for (let i = 0; i < this.accounts.length; i++) {
+      totalAssets += this.accounts[i].getBalance();
+    }
+  }
+  return totalAssets;
+};
