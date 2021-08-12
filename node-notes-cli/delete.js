01f7657239f725bd1d2108370be8eadc7f88ea72
Paul Park <paulpark330@hotmail.com>
2021-08-12 15:32:50 -0700

    deleted for in loops for delete.js and update.js

--- node-notes-cli/delete.js
+++ node-notes-cli/delete.js
@@ -1,13 +1,9 @@
 const fs = require('fs');
 
 module.exports = (notebook, input) => {
-  for (const key in notebook.notes) {
-    if (key === input) {
-      delete notebook.notes[key];
-      const notebookJSON = JSON.stringify(notebook, null, 2);
-      fs.writeFile('./data.json', notebookJSON, 'utf8', err => {
-        if (err) throw err;
-      });
-    }
-  }
+  delete notebook.notes[input];
+  const notebookJSON = JSON.stringify(notebook, null, 2);
+  fs.writeFile('./data.json', notebookJSON, 'utf8', err => {
+    if (err) throw err;
+  });
 };
--- node-notes-cli/update.js
+++ node-notes-cli/update.js
@@ -1,13 +1,9 @@
 const fs = require('fs');
 
 module.exports = (notebook, input, newNote) => {
-  for (const key in notebook.notes) {
-    if (key === input) {
-      notebook.notes[key] = newNote;
-      const notebookJSON = JSON.stringify(notebook, null, 2);
-      fs.writeFile('./data.json', notebookJSON, 'utf8', err => {
-        if (err) throw err;
-      });
-    }
-  }
+  notebook.notes[input] = newNote;
+  const notebookJSON = JSON.stringify(notebook, null, 2);
+  fs.writeFile('./data.json', notebookJSON, 'utf8', err => {
+    if (err) throw err;
+  });
 };
