138686223a6d4e21dcccda169354ff88e6dbc8ad
Paul Park <paulpark330@hotmail.com>
2021-08-26 19:17:34 -0700

    followed the instructions this time

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -3,22 +3,16 @@ import React from 'react';
 export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { opened: [] };
+    this.state = { opened: null };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(e) {
     const id = e.target.id;
-    const opened = this.state.opened;
-    if (opened.includes(id)) {
-      const index = opened.indexOf(id);
-      opened.splice(index, 1);
-      this.setState({ opened: opened });
+    if (this.state.opened === id) {
+      this.setState({ opened: null });
     } else {
-      this.setState(prevState => {
-        const list = [...prevState.opened, id];
-        return { opened: list };
-      });
+      this.setState({ opened: id });
     }
   }
 
@@ -32,7 +26,7 @@ export default class Accordion extends React.Component {
             <div id={topic.id} className="subject">
               {topic.subject}
             </div>
-            {this.state.opened.includes(topic.id) &&
+            {this.state.opened === topic.id &&
             <div className='details'>
               {topic.details}
             </div>
