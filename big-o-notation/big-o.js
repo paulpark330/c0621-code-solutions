6335e02e5144ccfe35d3e8bdfa431d1b5a190ed3
Paul Park <paulpark330@hotmail.com>
2021-09-21 15:28:36 -0700

    commi

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -9,9 +9,9 @@ function uniqueLinear(words) {
     i++                             // 2 * n = O(n)
   ) {
     const word = words[i];          // 2 * n = O(1)
-    if (!seen[word]) {              // 2 * 1 = O(1)
-      seen[word] = true;
-      unique[unique.length] = word; // 2 * 1 = O(1)
+    if (!seen[word]) {              // 3 * n = O(n)
+      seen[word] = true;            // 2 * n = O(n)
+      unique[unique.length] = word; // 2 * n = O(n)
     }
   }
   return unique;
@@ -28,16 +28,16 @@ function uniqueQuadratic(words) {
     let isUnique = true;            // 1 * 1 = O(1)
     for (
       let c = 0;                    // 1 * n = O(1)
-      c < i;                        // 1 * n^2 = O(n)
-      c++                           // 2 * n^2 = O(n)
+      c < i;                        // 1 * n^2 = O(n^2)
+      c++                           // 2 * n^2 = O(n^2)
     ) {
-      const comparing = words[c];   // 2 * n^2 = O(n)
-      if (comparing === word) {     // 1 * n = O(1)
-        isUnique = false;           // 1 * n = O(1)
+      const comparing = words[c];   // 2 * n^2 = O(n^2)
+      if (comparing === word) {     // 1 * n^2 = O(n^2)
+        isUnique = false;           // 1 * n^2 = O(n^2)
       }
     }
-    if (isUnique) {                 // 1 * 1 = O(1)
-      unique[unique.length] = word; // 2 * 1 = O(1)
+    if (isUnique) {                 // 1 * n = O(n)
+      unique[unique.length] = word; // 2 * n = O(n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
